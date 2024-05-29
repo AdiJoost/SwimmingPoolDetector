ca1d96aa47b9065dddd8805ff3880018a2fb3a33
danielschafi <daniel.schafi@bluewin.ch>
2024-05-29 19:42:02 +0200

    v2

--- vgg16AproachV2.py
+++ vgg16AproachV2.py
@@ -9,12 +9,20 @@ from sklearn.model_selection import train_test_split
 from dataGenerator import generateEqualDataSet
 from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.initializers import Constant
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator 
 import keras
 import pickle
 
+
+
+import numpy as np
+import imgaug as ia
+import imgaug.augmenters as iaa
+
+
+
 #Load Data Parameters
-NUMBER_OF_PICTURES = 231
+NUMBER_OF_PICTURES = 50
 TRUE_FALSE_RATIO = 0.1
 #Model Parameters
 NODES_AFTER_BASE_MODEL = 64
@@ -23,7 +31,7 @@ NODES_AFTER_BASE_MODEL = 64
 BATCH_SIZE = 36
 EPOCHS = 50
 EPOCHS_AFTER_UNFREEZING = 20
-CALLBACK = [ModelCheckpoint("vgg16_V5.keras", monitor='accuracy', verbose=1, save_best_only=True, mode='max'),]
+CALLBACK = [ModelCheckpoint("vgg16_V6.keras", monitor='accuracy', verbose=1, save_best_only=True, mode='max'),]
 METRICS = [Accuracy(), TruePositives(), TrueNegatives(), FalsePositives()]
 TEST_SIZE = 0.2
 
@@ -98,21 +106,51 @@ def getData(numberOfSamples, trueFalseRatio):
     return xTrain, xTest, yTrain, ytest
 
 def getDataGenerator(xTrain):
-    datagen = ImageDataGenerator(
-        rotation_range=ROTATION_RANGE,
-        width_shift_range=WIDTH_SHIFT_RANGE,
-        height_shift_range=HEIGTH_SHIFT_RANGE,
-        horizontal_flip=HORIZONTAL_FLIP,
-        vertical_flip=VERTICAL_FLIP
+    
+    seq = iaa.Sequential([
+    iaa.Fliplr(0.5), # horizontal flips
+    iaa.Crop(percent=(0, 0.1)), # random crops
+    # Small gaussian blur with random sigma between 0 and 0.5.
+    # But we only blur about 50% of all images.
+    iaa.Sometimes(
+        0.5,
+        iaa.GaussianBlur(sigma=(0, 0.5))
+    ),
+    # Strengthen or weaken the contrast in each image.
+    iaa.LinearContrast((0.75, 1.5)),
+    # Add gaussian noise.
+    # For 50% of all images, we sample the noise once per pixel.
+    # For the other 50% of all images, we sample the noise per pixel AND
+    # channel. This can change the color (not only brightness) of the
+    # pixels.
+    iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5),
+    # Make some images brighter and some darker.
+    # In 20% of all cases, we sample the multiplier once per channel,
+    # which can end up changing the color of the images.
+    iaa.Multiply((0.8, 1.2), per_channel=0.2),
+    # Apply affine transformations to each image.
+    # Scale/zoom them, translate/move them, rotate them and shear them.
+    iaa.Affine(
+        scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+        translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+        rotate=(-25, 25),
+        shear=(-8, 8)
     )
-    datagen.fit(xTrain)
-    return datagen
+    ], random_order=True) # apply augmenters in random order
+    
+    ig = ImageDataGenerator(preprocessing_function=seq.augment_image)  # pass this as the preprocessing function
+    
+    # gen = ig.flow_from_directory(xTrain)  # nothing else changes with the generator
+    
+    ig.fit(xTrain)
+    return ig
 
+    
 def trainModel(model, xTrain, yTrain, datagen, batchSize, epochs, callback=None):
     classWeights = _getClassWeigths(xTrain)
     history = model.fit(
         datagen.flow(xTrain, yTrain, batch_size=batchSize),
-        steps_per_epoch= len(xTrain) / batchSize,
+        steps_per_epoch= int(len(xTrain) / batchSize),
         epochs=epochs,
         callbacks=callback,
         class_weight = classWeights)
